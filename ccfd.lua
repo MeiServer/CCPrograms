a0e06ff9099b4b4f5b9c2a28a015469c0c947521
kouch0828 <kokou.1031@outlook.jp>
2022-11-01 23:43:07 +0900

    add ccfd.lua

---
+++ ccfd.lua
@@ -0,0 +1,35 @@
+local args = {...}
+
+local helpText = "usage: ccfd <FileName> <URL>"
+
+function parseArgs(args)
+	local obj = {
+		outputFileName = assert(args[1], helpText),
+		downloadUrl = assert(args[2], helpText)
+	}
+	return obj;
+end
+
+function openFile(fileName)
+	local fileHandler = fs.open(fileName, "w")
+	
+	return assert(fileHandler, "Failed to open file: " .. fileName)
+end
+
+function openRemoteFile(url)
+	local remoteFileHandler = http.get(url)
+	
+	return assert(remoteFileHandler, "Failed to open url: " .. url)
+end
+
+local parsedArgs = parseArgs(args)
+
+local file = openFile(parsedArgs.outputFileName)
+local remoteFile = openRemoteFile(parsedArgs.downloadUrl)
+
+file.write(remoteFile.readAll())
+file.flush()
+file.close();
+remoteFile.close()
+
+print("done!")
