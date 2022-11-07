a0dc3c21e2def09d45eadf536eee9630e6c488c9
niko0229 <84462001+niko0229@users.noreply.github.com>
2022-11-08 02:46:47 +0900

    初期描画の色が変化する不具合の修正

--- ctc/demo.lua
+++ ctc/demo.lua
@@ -6,7 +6,7 @@ local final limit = 10
 
 --##Function##
 function drawBlockage(mon)
-	paintutils.drawPixel(1, 1, 128)
+	term.setBackgoundColor(colors.gray)
 	local ig = paintutils.loadImage(image)
 	paintutils.drawImage(ig, 1, 1)
 end
@@ -166,6 +166,7 @@ while rs.getInput("back") do
 		sleep(Time)
 		
 		if not rs.getInput("back") then
+			term.setBackgoundColor(colors.gray)
 			term.restore()
 			return
 		end
@@ -173,5 +174,5 @@ while rs.getInput("back") do
 	sleep(0)
 end
 
-paintutils.drawPixel(1, 1, 128)
+term.setBackgoundColor(colors.gray)
 term.restore()
